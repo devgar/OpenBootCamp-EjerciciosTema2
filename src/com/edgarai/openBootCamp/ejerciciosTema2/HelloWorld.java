b8feafe5d6788b098b8cd38a11dfb568dad8b2b0
Edgar Albalate <dev.gardo@gmail.com>
2022-07-08 13:57:12 +0200

    Implementa la función 'calcularPrecioConIVA'

--- src/com/edgarai/openBootCamp/ejerciciosTema2/HelloWorld.java
+++ src/com/edgarai/openBootCamp/ejerciciosTema2/HelloWorld.java
@@ -1,6 +1,19 @@
 package com.edgarai.openBootCamp.ejerciciosTema2;
 
 public class HelloWorld {
+
+    /**
+     * Función que devuelve el precio con el IVA incluido.
+     * @param amount Precio neto a partir del cual calcular el total (IVA incluido)
+     */
+    static float calcularPrecioConIVA(float amount) {
+        return amount * 1.21f;
+    }
+
+    /**
+     * Función principal. Simplemente imprime 'Hello World!'
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
